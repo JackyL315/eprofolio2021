d629f69b398d8a83058295c7000e517151508580
JackyLiang <jacky.liang@silksoftware.com>
2021-07-25 00:05:29 -0400

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,7 +36,7 @@
 				var $nav = $header.children('nav'),
 				$nav_li = $nav.find('li');
 				if ($nav_li.length % 2 == 0) {
-					console.log($nav_li.length)
+					//console.log($nav_li.length)
 					$nav.addClass('use-middle');
 					$nav_li.eq( ($nav_li.length / 2) ).addClass('is-middle');
 
