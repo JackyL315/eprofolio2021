81c0d8540c82e2a5a30d36b0f0a63de38667b2c6
JackyLiang <jacky.liang@silksoftware.com>
2021-07-24 22:52:51 -0400

    update

--- blocks/global/footer.php
+++
@@ -1,4 +0,0 @@
-<!-- Footer -->
-	<footer id="footer">
-		<p class="copyright">&copy; JiaQi Liang Design, All rights reserved</p>
-	</footer>
--- blocks/intro.php
+++
@@ -1,5 +0,0 @@
-<article id="intro">
-	<h2 class="major">Intro</h2>
-	<span class="image main"><img src="images/hello.jpg" alt="" /></span>
-	<p>I had worked as a Front-end Developer with 3+ years of experience on E-commerce with multiple platforms (Magento 2, Shopify and BigCommerce) and WordPress. Experience in develop and maintain website using HTML, CSS, JavaScript, jQuery, and Bootstrap, able to identified and corrected problems uncovered during testing or customer feedback under Tech lead’s guidance. Also, able to create documents of extended guide for modules with more user-friendly content and documents of bug report for teammates. </p>
-</article>
